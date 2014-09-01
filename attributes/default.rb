63b6e359f6ef292f2bcc056dfb7fdc5f7e3c7814
David Baena <baena5999@gmail.com>
2014-09-02 01:25:28 +0200

    fixed atributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 default['varnish']['backend_host']="www.resultados-futbol.com"
 default['varnish']['backend_port']="80"
-node['varnish']['group']="root"
-node['varnish']['user']="root"
+default['varnish']['group']="root"
+default['varnish']['user']="root"
